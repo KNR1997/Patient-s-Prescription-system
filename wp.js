38ebd837c8f03f26542c3404b104b8f7d80ee216
Kethaka Ranasinghe <kethakaranasinghe@gmail.com>
2022-11-09 14:51:36 +0530

    Update wp.js

--- wp.js
+++ wp.js
@@ -4,3 +4,19 @@ const patient = document.querySelector('.patient');
 search.addEventListener('click', function() {
     patient.classList.remove('hidden');
 });
+/* Add New button*/
+const popup = document.querySelector('#pop-up');
+const addNew = document.querySelector('.addnew');
+const enter = document.querySelector('.popup-enter');
+const close = document.querySelector('.popup-close');
+
+addNew.addEventListener('click',function(){
+    popup.style.display = 'flex';
+});
+
+enter.addEventListener('click', function(){
+    popup.style.display = 'none';
+});
+close.addEventListener('click', function(){
+    popup.style.display = 'none';
+})
